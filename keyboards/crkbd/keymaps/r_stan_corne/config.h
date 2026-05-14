6be7245c8420630771bf78fa83d88479bdfec895
Raphael <68848614+r-2tanz4n1@users.noreply.github.com>
2026-05-14 17:22:27 +0100

    Trying to create a QMK firmware for my Corne 4_1

---
+++ keyboards/crkbd/keymaps/r_stan_corne/config.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define MASTER_RIGHT
+
+#define TAPPING_TERM 145
+#define QUICK_TAP_TERM 110
+#define FLOW_TAP_TERM 80
+#define CHORDAL_HOLD
+
+#define COMBO_TERM 50
+#define COMBO_MUST_HOLD_MODS
+#define COMBO_TERM_PER_COMBO
+
+#define AUTO_SHIFT_TIMEOUT 110
+#define NO_AUTO_SHIFT_SPECIAL
+
+#define CAPS_WORD_IDLE_TIMEOUT 5000
+
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 50
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE
+#define RGB_MATRIX_KEYPRESSES // enable reactive effects on keypress
+#define RGB_MATRIX_SLEEP // allow matrix to turn off on suspend if desired
+
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+#define IGNORE_MOD_TAP_INTERRUPT
