7e16405f1e9490dfdff89d6a077f8d2cd61e76db
Raphael <68848614+r-2tanz4n1@users.noreply.github.com>
2026-05-14 22:33:56 +0100

    Implement key combos for enhanced functionality
    Added combo definitions and key combinations for various key events.

--- keyboards/crkbd/keymaps/r_stan_corne/keymap.c
+++ keyboards/crkbd/keymaps/r_stan_corne/keymap.c
@@ -16,6 +16,126 @@ enum layers {
 #define FN_BKS LT(_FN, KC_BSPC)
 
 
+/* -------------------- COMBOS -------------------- */
+
+enum combo_events {
+    CB_1,
+    CB_2,
+    CB_3,
+    CB_4,
+    CB_5,
+    CB_6,
+    CB_7,
+    CB_8,
+    CB_9,
+    CB_0,
+    CB_AT,
+    CB_EURO,
+    CB_LBRC,
+    CB_RBRC,
+    CB_HASH,
+    CB_EXLM,
+    CB_QUES,
+    CB_SLSH,
+    CB_CAPS_WORD,
+    CB_LT,
+    CB_GT,
+    CB_LPRN,
+    CB_RPRN,
+    CB_QUOT,
+    CB_DQUO,
+    CB_ASTR,
+    CB_PLUS,
+    CB_FN_L,
+    CB_FN_R,
+    CB_NUM_ON,
+    CB_NUM_OFF,
+    COMBO_LENGTH
+};
+
+uint16_t COMBO_LEN = COMBO_LENGTH;
+
+/* BASE layer combos */
+const uint16_t PROGMEM cb_1[]         = {KC_TAB,  KC_Q,    COMBO_END};
+const uint16_t PROGMEM cb_2[]         = {KC_Q,    KC_W,    COMBO_END};
+const uint16_t PROGMEM cb_3[]         = {KC_W,    KC_E,    COMBO_END};
+const uint16_t PROGMEM cb_4[]         = {KC_E,    KC_R,    COMBO_END};
+const uint16_t PROGMEM cb_5[]         = {KC_R,    KC_T,    COMBO_END};
+
+const uint16_t PROGMEM cb_6[]         = {KC_Y,    KC_U,    COMBO_END};
+const uint16_t PROGMEM cb_7[]         = {KC_U,    KC_I,    COMBO_END};
+const uint16_t PROGMEM cb_8[]         = {KC_I,    KC_O,    COMBO_END};
+const uint16_t PROGMEM cb_9[]         = {KC_O,    KC_P,    COMBO_END};
+const uint16_t PROGMEM cb_0[]         = {KC_P,    KC_RBRC, COMBO_END};
+
+const uint16_t PROGMEM cb_at[]        = {KC_A,    KC_S,    COMBO_END};
+const uint16_t PROGMEM cb_euro[]      = {KC_S,    KC_D,    COMBO_END};
+const uint16_t PROGMEM cb_lbrc[]      = {KC_D,    KC_F,    COMBO_END};
+const uint16_t PROGMEM cb_rbrc[]      = {KC_F,    KC_G,    COMBO_END};
+
+const uint16_t PROGMEM cb_hash[]      = {KC_H,    KC_J,    COMBO_END};
+const uint16_t PROGMEM cb_exlm[]      = {KC_J,    KC_K,    COMBO_END};
+const uint16_t PROGMEM cb_ques[]      = {KC_K,    KC_L,    COMBO_END};
+const uint16_t PROGMEM cb_slsh[]      = {KC_L,    HM_SCLN, COMBO_END};
+
+const uint16_t PROGMEM cb_caps_word[] = {KC_LSFT, KC_Z,    COMBO_END};
+const uint16_t PROGMEM cb_lt[]        = {KC_Z,    KC_X,    COMBO_END};
+const uint16_t PROGMEM cb_gt[]        = {KC_X,    KC_C,    COMBO_END};
+const uint16_t PROGMEM cb_lprn[]      = {KC_C,    KC_V,    COMBO_END};
+const uint16_t PROGMEM cb_rprn[]      = {KC_V,    KC_B,    COMBO_END};
+
+const uint16_t PROGMEM cb_quot[]      = {KC_N,    KC_M,    COMBO_END};
+const uint16_t PROGMEM cb_dquo[]      = {KC_M,    KC_COMM, COMBO_END};
+const uint16_t PROGMEM cb_astr[]      = {KC_COMM, KC_DOT,  COMBO_END};
+const uint16_t PROGMEM cb_plus[]      = {KC_DOT,  KC_SLSH, COMBO_END};
+
+const uint16_t PROGMEM cb_fn_l[]      = {LALT_T(KC_F2),    LGUI_T(KC_SPACE), COMBO_END};
+const uint16_t PROGMEM cb_fn_r[]      = {RGUI_T(KC_BSPC),  NAV_SPC,          COMBO_END};
+const uint16_t PROGMEM cb_num_on[]    = {NAV_SPC,          HYPR_T(KC_DEL),   COMBO_END};
+const uint16_t PROGMEM cb_num_off[]   = {KC_SPC,           S(KC_0),          COMBO_END};
+
+
+combo_t key_combos[COMBO_LENGTH] = {
+    [CB_1]         = COMBO(cb_1, KC_1),
+    [CB_2]         = COMBO(cb_2, KC_2),
+    [CB_3]         = COMBO(cb_3, KC_3),
+    [CB_4]         = COMBO(cb_4, KC_4),
+    [CB_5]         = COMBO(cb_5, KC_5),
+
+    [CB_6]         = COMBO(cb_6, KC_6),
+    [CB_7]         = COMBO(cb_7, KC_7),
+    [CB_8]         = COMBO(cb_8, KC_8),
+    [CB_9]         = COMBO(cb_9, KC_9),
+    [CB_0]         = COMBO(cb_0, KC_0),
+
+    [CB_AT]        = COMBO(cb_at,   LALT(KC_2)),
+    [CB_EURO]      = COMBO(cb_euro, LALT(KC_3)),
+    [CB_LBRC]      = COMBO(cb_lbrc, LALT(KC_8)),
+    [CB_RBRC]      = COMBO(cb_rbrc, LALT(KC_9)),
+
+    [CB_HASH]      = COMBO(cb_hash, S(KC_3)),
+    [CB_EXLM]      = COMBO(cb_exlm, S(KC_1)),
+    [CB_QUES]      = COMBO(cb_ques, S(KC_MINUS)),
+    [CB_SLSH]      = COMBO(cb_slsh, S(KC_7)),
+
+    [CB_CAPS_WORD] = COMBO(cb_caps_word, CAPS_WORD),
+    [CB_LT]        = COMBO(cb_lt, KC_NUBS),
+    [CB_GT]        = COMBO(cb_gt, S(KC_NUBS)),
+    [CB_LPRN]      = COMBO(cb_lprn, S(KC_8)),
+    [CB_RPRN]      = COMBO(cb_rprn, S(KC_9)),
+
+    [CB_QUOT]      = COMBO(cb_quot, KC_MINUS),
+    [CB_DQUO]      = COMBO(cb_dquo, S(KC_2)),
+    [CB_ASTR]      = COMBO(cb_astr, S(KC_EQL)),
+    [CB_PLUS]      = COMBO(cb_plus, KC_EQL),
+
+    [CB_FN_L]      = COMBO(cb_fn_l, MO(_FN)),
+    [CB_FN_R]      = COMBO(cb_fn_r, MO(_FN)),
+    [CB_NUM_ON]    = COMBO(cb_num_on, TO(_NUM)),
+    [CB_NUM_OFF]   = COMBO(cb_num_off, TO(_BASE)),
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
