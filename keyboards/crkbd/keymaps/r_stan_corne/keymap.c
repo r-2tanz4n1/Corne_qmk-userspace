c0e973a81bd4aae623ee13dd4782c7318a84d241
Raphael <68848614+r-2tanz4n1@users.noreply.github.com>
2026-05-15 06:46:40 +0100

    Enable matrix print for debugging
    Add debug print for matrix state if DEBUG is defined

--- keyboards/crkbd/keymaps/r_stan_corne/keymap.c
+++ keyboards/crkbd/keymaps/r_stan_corne/keymap.c
@@ -186,3 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+#ifdef DEBUG
+    matrix_print();
+#endif
